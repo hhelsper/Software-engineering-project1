6a83ceb933591cda5007bc541425f2d2887f59a6
hhelsper <89586701+hhelsper@users.noreply.github.com>
2022-02-24 08:38:45 -0500

    working through bugs

--- main.py
+++ main.py
@@ -1,47 +1,226 @@
 import os
 from tmdb_api_call import get_movie_data
-from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    login_required,
+    current_user,
+    logout_user,
+)
+from flask import Flask, render_template, request, Blueprint, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import find_dotenv, load_dotenv
 
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+load_dotenv(find_dotenv())
+# Point SQLAlchemy to your Heroku database
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+# Gets rid of a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+login_manager = LoginManager()
+login_manager.login_view = "hello_world"
+login_manager.init_app(app)
+
+db = SQLAlchemy(app)
 
 
+class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
-@app.route('/', methods=["POST", "GET"])
+    def __repr__(self):
+        return "<User %r" % self.user_name
+
+    def get_username(self):
+        return self.user_name
+
+    def get_id(self):
+        return self.user_id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Rating(db.Model):
+    __tablename__ = "rating"
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.Integer, db.ForeignKey("user.user_name"))
+    rating = db.Column(db.Integer, nullable=False)
+    movie_name = db.Column(db.String(120), nullable=False)
+
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.Integer, db.ForeignKey("user.user_name"))
+    comment = db.Column(db.String(300), nullable=False)
+    movie_name = db.Column(db.String(120), nullable=False)
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    wiki_link = db.Column(db.String(300), nullable=False)
+
+
+db.create_all()
+
+
+@app.route("/", methods=["POST", "GET"])
 def hello_world():
-    """ Returns root endpoint HTML """
+    """Returns root endpoint HTML"""
+
+    return render_template(
+        "login.html",
+    )
+
+
+@app.route("/home_page", methods=["POST", "GET"])
+@login_required
+def home_page():
+    """Returns root endpoint HTML"""
     if request.method == "POST":
         chosen_movie = request.form.get("chosen_movie")
     else:
-        chosen_movie = ''
+        chosen_movie = ""
+
+    movie_data = get_movie_data(chosen_movie)
+
+    print(chosen_movie)
+    return render_template(
+        "index.html",
+        movie_id=movie_data["movie_id"],
+        name=movie_data["name"],
+        overview=movie_data["overview"],
+        genres=movie_data["genre"],
+        img_url=movie_data["image_url"],
+        wiki_link=movie_data["wiki_link"],
+        similar_movie_ids=movie_data["similar_movie_ids"],
+        similar_movies=movie_data["similar_movies"],
+        similar_posters=movie_data["similar_posters"],
+        user_name=current_user.user_name,
+    )
 
 
+@app.route("/home_page_reload", methods=["POST", "GET"])
+@login_required
+def home_page_reload():
+    """Returns root endpoint HTML"""
+
+    chosen_movie = request.form.get("movie_id")
+
+    print("chosen movie:" + request.form.get("movie_id"))
     movie_data = get_movie_data(chosen_movie)
 
     return render_template(
         "index.html",
-        name =movie_data['name'],
-        overview =movie_data['overview'],
-        genres = movie_data['genre'],
-        img_url = movie_data['image_url'],
-        wiki_link = movie_data['wiki_link'],
-        similar_movie_ids = movie_data['similar_movie_ids'],
-        similar_movies = movie_data['similar_movies'],
-        similar_posters = movie_data['similar_posters'],
-        )
-
-
-@app.route('/wiki')
-def wiki_page():
-    """ Returns wiki endpoint HTML """
-    return render_template(
-        "wiki.html",
+        movie_id=chosen_movie,
+        name=movie_data["name"],
+        overview=movie_data["overview"],
+        genres=movie_data["genre"],
+        img_url=movie_data["image_url"],
+        wiki_link=movie_data["wiki_link"],
+        similar_movie_ids=movie_data["similar_movie_ids"],
+        similar_movies=movie_data["similar_movies"],
+        similar_posters=movie_data["similar_posters"],
+        user_name=current_user.user_name,
     )
 
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', '8080')),
-    debug=True
-)
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    """Adds user to database if not already in it and returns main page"""
+    if request.method == "POST":
+        user_name = request.form.get("user_name")
+        password = request.form.get("password")
+
+    user = User.query.filter_by(user_name=user_name).first()
+
+    if user:
+        flash("Email address already exists. Try logging in")
+        return redirect(url_for("login"))
+
+    new_user = User(
+        user_name=user_name, password=generate_password_hash(password, method="sha256")
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/login")
+
+
+@app.route("/login")
+def login():
+    """Return main page after successful login"""
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    # login code goes here
+    if request.form.get("signup") == "signup":
+        return render_template("signup.html")
+
+    user_name = request.form.get("user_name")
+    password = request.form.get("password")
+    remember = True if request.form.get("remember") else False
+
+    user = User.query.filter_by(user_name=user_name).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash("Please check your login details and try again.")
+        return redirect(url_for("login"))
+
+    movie_data = get_movie_data("")
+
+    login_user(user, remember=remember)
+
+    return redirect("/home_page")
+
+
+@app.route("/wiki")
+@login_required
+def wiki_page():
+    """Returns wiki endpoint HTML"""
+    return render_template("wiki.html")
+
+
+# @app.route("/comments", methods=["GET", "POST"])
+# @login_required
+# def comments():
+#     """Returns wiki endpoint HTML"""
+
+#     if request.method == "POST":
+#         data = request.form
+#         comment = Comment(
+#             comment=data["comment"],
+#             wiki_link=data["wiki_link"],
+#             movie_name=data["movie_name"],
+#             user_name=current_user.user_name,
+#         )
+#     return render_template(
+#         "comments.html",
+#     )
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8080")), debug=True
+    )
--- tmdb_api_call.py
+++ tmdb_api_call.py
@@ -5,12 +5,13 @@ from dotenv import load_dotenv, find_dotenv
 
 from wiki import get_wiki_link
 
-load_dotenv(find_dotenv()) # This is to load your API keys from .env
+load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
-BASE_URL = 'https://api.themoviedb.org/3/movie/'
+BASE_URL = "https://api.themoviedb.org/3/movie/"
+
 
 def get_similar_movies(chosen_movie, params):
-    """ Gets similar movies to passed in movie id from TMDB server """
+    """Gets similar movies to passed in movie id from TMDB server"""
     response = requests.get(BASE_URL + chosen_movie + "/similar", params=params)
     data = response.json()
     similar_movie_ids = []
@@ -18,52 +19,55 @@ def get_similar_movies(chosen_movie, params):
     similar_posters = []
 
     for i in range(4):
-        similar_movie_ids.append(data['results'][i]['id'])
-        similar_movies.append(data['results'][i]['original_title'])
+        similar_movie_ids.append(data["results"][i]["id"])
+        similar_movies.append(data["results"][i]["original_title"])
         similar_posters.append(
-            'https://image.tmdb.org/t/p/w185' +
-            data['results'][i]['poster_path']
-            )
+            "https://image.tmdb.org/t/p/w185" + data["results"][i]["poster_path"]
+        )
 
     return similar_movie_ids, similar_movies, similar_posters
 
 
 def get_movie_data(chosen_movie):
-    """ Chooses randomly from list of movie ids to do a get from TMDB's server """
+    """Chooses randomly from list of movie ids to do a get from TMDB's server"""
 
     movie_ids = [
-        '157336',
-        '12445',
-        '9476',
-        '157350',
-        '262504',
-        '262500',
-        '70160',
-        '101299',
-        '198663'
+        "157336",
+        "12445",
+        "9476",
+        "157350",
+        "262504",
+        "262500",
+        "70160",
+        "101299",
+        "198663",
     ]
     params = {
-        'api_key': os.getenv('TMDB_KEY'),
+        "api_key": os.getenv("TMDB_KEY"),
     }
-    if chosen_movie == '':
+    if chosen_movie != "":
+        chosen_movie = chosen_movie
+    elif chosen_movie == "":
         chosen_movie = random.choice(movie_ids)
 
     response = requests.get(BASE_URL + chosen_movie, params=params)
     data = response.json()
 
+    wiki_link = get_wiki_link(data["original_title"])
+    similar_movie_ids, similar_movies, similar_posters = get_similar_movies(
+        chosen_movie, params
+    )
 
-    wiki_link = get_wiki_link(data['original_title'])
-    similar_movie_ids, similar_movies, similar_posters = get_similar_movies(chosen_movie, params)
-
-    genre_list = data['genres']
-    genre_names = [d['name'] for d in genre_list]
+    genre_list = data["genres"]
+    genre_names = [d["name"] for d in genre_list]
     return {
-        'name': data['original_title'],
-        'overview': data['tagline'],
-        'genre': genre_names,
-        'image_url': 'https://image.tmdb.org/t/p/w500' + data['poster_path'],
-        'wiki_link': wiki_link,
-        'similar_movie_ids': similar_movie_ids,
-        'similar_movies': similar_movies,
-        'similar_posters': similar_posters,
+        "movie_id": chosen_movie,
+        "name": data["original_title"],
+        "overview": data["tagline"],
+        "genre": genre_names,
+        "image_url": "https://image.tmdb.org/t/p/w500" + data["poster_path"],
+        "wiki_link": wiki_link,
+        "similar_movie_ids": similar_movie_ids,
+        "similar_movies": similar_movies,
+        "similar_posters": similar_posters,
     }
