1e8169a3a7f331c16c96819f721e340e33a7a14d
hhelsper <89586701+hhelsper@users.noreply.github.com>
2022-02-25 08:55:14 -0500

    wtf

--- main.py
+++ main.py
@@ -277,5 +277,5 @@ def logout():
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8080")), debug=True
     )
