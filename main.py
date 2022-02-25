e6a5f7c47a90ba7b1eeef290478a7fb918b0fd2e
hhelsper <89586701+hhelsper@users.noreply.github.com>
2022-02-25 06:31:54 -0500

    trying to fix heroku bug

--- main.py
+++ main.py
@@ -275,5 +275,7 @@ def logout():
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8080")), debug=True
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", int(8080))),
+        debug=True,
     )
